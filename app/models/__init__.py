8b3bb561f922d6c67efe0e825585abed379f5fbc
vivitruong <vivitruong1506@gmail.com>
2023-08-26 13:10:25 -0500

    add seeding data for watchlist

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db
 from .user import User
+from .watchlist import Watchlist
+from .portfolio import Portfolio
+from .watchlist_stock import Watchlist_Stock
+from .news import News
+from .stockSymbol import StockSymbol
+from .transaction import Transaction
 from .db import environment, SCHEMA
