0527cfc01329f8b42d60057d7dd9ba50941fc70c
vivitruong <vivitruong1506@gmail.com>
2023-08-29 16:42:09 -0500

    need to test route

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -38,3 +38,49 @@ def create_watchlist():
             return {'error': 'there is an error in form.validate'}
     if form.errors:
         return {'errors': form.errors}
+
+#update wathclist
+@watchlist_routes.route('/<int:watchlist_id>', methods=['PUT'])
+@login_required
+def update_watchlist(watchlist_id):
+    current_user_info = current_user.to_dict()
+    current_user_id = current_user_info['id']
+    update_watchlist = Watchlist.query.get(watchlist_id)
+    if update_watchlist:
+        if update_watchlist.user_id == current_user_id:
+            data = request.get_json()
+            update_watchlist.name = data['name']
+            db.session.commit()
+            return update_watchlist.to_dict(), 200
+        else:
+            return {'error': {
+                'message': 'Forbidden',
+                'statusCode': 403
+            }}, 403
+    else:
+        return{'error': {
+            'message': 'Wathclist does not exist',
+            'statusCode': 404
+        }}, 404
+
+#delete a wathclist
+@watchlist_routes.route('/<int:watchlist_id>', methods=['DELETE'])
+def delete_watchlist(watchlist_id):
+    current_user_info = current_user.to_dict()
+    current_user_id = current_user_info['id']
+    delete_watchlist = Watchlist.query.get(watchlist_id)
+    if delete_watchlist:
+        if delete_watchlist.user_id == current_user_id:
+            db.session.delete(delete_watchlist)
+            db.session.commit()
+            return {'message': 'Successfully delete'}
+        else:
+            return {'error': {
+                'message': 'Forbidden',
+                'statusCode': 403
+            }}, 403
+    else:
+        return {'error': {
+            'message': 'Can not find watchlist',
+            'statusCode': 404
+        }}, 404
