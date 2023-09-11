be75a3c808cfb15ba1432ead29933f9d6a767725
TroyRandall <troyrandall18@gmail.com>
2023-09-10 23:39:48 -0400

    final touches on purchase and sell stock forms functionality.

--- react-app/src/components/StockSellForm/index.js
+++ react-app/src/components/StockSellForm/index.js
@@ -1,94 +1,98 @@
-import { useState, useEffect} from "react";
-import { useDispatch, useSelector,   } from "react-redux";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { authenticate } from "../../store/session";
 
 import * as portfolioActions from "../../store/portfolio";
-import './StockSellForm.css'
+import "./StockSellForm.css";
 
 function SellStockForm({ portfolio }) {
   const dispatch = useDispatch();
-  const { userId } = useParams()
+  const { userId } = useParams();
   const currentUser = useSelector((state) => state.session.user);
-  const stockInfo = useSelector((state) => state.stocks)
+  const stockInfo = useSelector((state) => state.stocks);
   const portfolios = useSelector((state) => state.portfolios[userId]);
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
   const [toggle, setToggle] = useState(false);
   const [errors, setErrors] = useState({});
-  const [portfolioId, setPortfolioId] = useState(portfolio.id)
+  const [backendToggle, setBackendToggle] = useState(false);
+  const [portfolioId, setPortfolioId] = useState(portfolio.id);
   const [tickerSymbol, setTickerSymbol] = useState(portfolio.symbol);
   const [quantity, setQuantity] = useState("");
   const [avgPrice, setAvgPrice] = useState("");
   const [estimate, setEstimate] = useState("");
   const [submitToggle, setSubmitToggle] = useState(false);
 
-
   useEffect(() => {
-
     setEstimate((avgPrice * quantity).toFixed(2));
-    if(portfolio.symbol) setIsLoaded(true)
+    if (portfolio.symbol) setIsLoaded(true);
     if (submitToggle) {
       let newErrors = {};
 
-        if (quantity <= 0) newErrors.quantity = "Quantity is Required";
-        if (quantity > portfolio.quantity && !newErrors.quantity) newErrors.quantity = 'You cannot Sell More Shares Than You Own'
-        if (tickerSymbol === "") newErrors.ticker = "Ticker Symbol is required";
-        if (avgPrice <= 0) newErrors.price = "Price is Required";
-
+      if (quantity <= 0) newErrors.quantity = "Quantity is Required";
+      if (quantity > portfolio.quantity && !newErrors.quantity)
+        newErrors.quantity = "You cannot Sell More Shares Than You Own";
+      if (tickerSymbol === "") newErrors.ticker = "Ticker Symbol is required";
+      if (Number(avgPrice) <= 0) newErrors.price = "Price is Required";
 
       setErrors({ ...newErrors });
       if (errors && Object.values(newErrors).length > 0) return errors;
+      setBackendToggle(true);
     }
-
   }, [avgPrice, quantity, submitToggle, portfolios]);
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e?.preventDefault();
 
     setSubmitToggle(true);
-    let id = currentUser?.id;
-    let sold_at = new Date()
-    let portfolio = { id, tickerSymbol, quantity, avgPrice, sold_at, portfolioId};
-    if (!errors.length) {
-      const response = await dispatch(
-        portfolioActions.updatePortfolioItem(portfolio)
-      ).catch(async (res) => {
-        const data = res;
-        if (data && data.errors) setErrors(data.errors);
-      });
-      if(errors.length){
-        setToggle(false)
-        setSubmitToggle(false);
+    if (backendToggle) {
+      let id = currentUser?.id;
+      let sold_at = new Date();
+      let portfolio = {
+        id,
+        tickerSymbol,
+        quantity,
+        avgPrice,
+        sold_at,
+        portfolioId,
+      };
+      if (!errors.length) {
+        const response = await dispatch(
+          portfolioActions.updatePortfolioItem(portfolio)
+        ).catch(async (res) => {
+          const data = res;
+          if (data && data.errors) setErrors(data.errors);
+        });
+        await dispatch(portfolioActions.getPortfoliosByUser(userId));
+        await dispatch(authenticate());
+        return response;
       }
-      await dispatch(portfolioActions.getPortfoliosByUser(userId))
-      await dispatch(authenticate())
-      return response;
     }
   };
 
   const cancelModal = (e) => {
-    const overlay = document.getElementById('overlay');
-    const yesButton = document.getElementById('yes-button')
-    const noButton = document.getElementById('no-button')
-    if(e.target !== overlay && e.target !== noButton) return null
+    const overlay = document.getElementById("overlay");
+    const yesButton = document.getElementById("yes-button");
+    const noButton = document.getElementById("no-button");
+    if (e.target === overlay || e.target === noButton) {
+      setToggle(false);
+      setSubmitToggle(false);
+      setErrors(false)
+    return null}
     else if (e.target === yesButton) {
       let newErrors = {};
 
-        if (quantity <= 0) newErrors.quantity = "Quantity is Required";
-        if (quantity > portfolio.quantity && !newErrors.quantity) newErrors.quantity = 'You cannot Sell More Shares Than You Own'
-        if (tickerSymbol === "") newErrors.ticker = "Ticker Symbol is required";
-        if (avgPrice <= 0) newErrors.price = "Price is Required";
-
+      if (quantity <= 0) newErrors.quantity = "Quantity is Required";
+      if (quantity > portfolio.quantity && !newErrors.quantity)
+        newErrors.quantity = "You cannot Sell More Shares Than You Own";
+      if (tickerSymbol === "") newErrors.ticker = "Ticker Symbol is required";
+      if (Number(avgPrice) <= 0) newErrors.price = "Price is Required";
 
       setErrors({ ...newErrors });
       if (errors && Object.values(newErrors).length > 0) return errors;
-      else return handleSubmit()
-    } else {
-    setToggle(false)
-    setSubmitToggle(false);
+      else return handleSubmit();
     }
-
-  }
+  };
 
   const UlClassName = "overlay" + (toggle ? "" : "hidden");
 
@@ -98,40 +102,60 @@ function SellStockForm({ portfolio }) {
   const checkModal = () => {
     if (toggle) {
       return (
-        <div className={UlClassName} onClick={cancelModal} id='overlay'>
-          <div id="sell-stock-form">
-            {Object.values(errors).length ? Object.values(errors).map((error) => {
-              return <div>{}</div>
-            }) : ''}
+        <div className={UlClassName} onClick={cancelModal} id="overlay">
+          <div id="sell-stock-form-container">
+            {Object.values(errors).map((error) => {
+              return <div id='sell-errors'>{error}</div>;
+            })}
             How Many Shares of {portfolio.name} Would You Like To Sell?
             <form id="sell-stock-form">
               <label>
                 {" "}
                 Quantity
                 <input
-                 id="sell-form-quantity-input"
-                 name="quantity"
-                 placeholder="Quantity"
-                 value={quantity}
-                 onChange={(e) => setQuantity(e.target.value)}
-                 required>
-                </input>
+                  id="sell-form-quantity-input"
+                  name="quantity"
+                  placeholder="Quantity"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                  required
+                ></input>
               </label>
-
-              <label>Price
-              <input
-            id="sell-form-price-input"
-            name="Average price"
-            placeholder={stockInfo[portfolio.symbol]['Time Series (Daily)'][stockInfo[portfolio.symbol]['Time Series (Daily)'].length -1]}
-            value={avgPrice}
-            onChange={(e) => setAvgPrice(e.target.value)}
-            required
-            default={stockInfo[portfolio.symbol]['Time Series (Daily)'][stockInfo[portfolio.symbol]['Time Series (Daily)'].length -1]}
-          ></input>
+              <label>
+                Price
+                <input
+                  id="sell-form-price-input"
+                  name="Average price"
+                  placeholder={
+                    stockInfo[portfolio.symbol]["Time Series (Daily)"][
+                      stockInfo[portfolio.symbol]["Time Series (Daily)"]
+                        .length - 1
+                    ]
+                  }
+                  value={avgPrice}
+                  onChange={(e) => setAvgPrice(e.target.value)}
+                  required
+                  default={
+                    stockInfo[portfolio.symbol]["Time Series (Daily)"][
+                      stockInfo[portfolio.symbol]["Time Series (Daily)"]
+                        .length - 1
+                    ]
+                  }
+                ></input>
               </label>
-
-              <div>Are You Sure You Would Like to Sell {quantity} shares of {portfolio.name} for {estimate}?
-              <button onClick={handleSubmit} id='yes-button'>Yes</button> <button onClick={cancelModal} id='no-button'>No</button></div>
+              <div>
+                Are You Sure You Would Like to Sell {quantity} shares of{" "}
+                {portfolio.name} for {estimate}?
+                <div>
+                  {" "}
+                  <button onClick={handleSubmit} id="yes-button">
+                    Yes
+                  </button>{" "}
+                  <button onClick={cancelModal} id="no-button">
+                    No
+                  </button>
+                </div>
+              </div>{" "}
             </form>
           </div>
         </div>
@@ -139,7 +163,7 @@ function SellStockForm({ portfolio }) {
     }
   };
 
-  return ( portfolio?.quantity === 0 || portfolio?.sold_at ? null:
+  return portfolio?.quantity === 0 || portfolio?.sold_at ? null : (
     <div id="stock-asset-container">
       <div className="stock-asset-item2">{portfolio.name}</div>
       <div className="stock-asset-item2">{portfolio.symbol}</div>
