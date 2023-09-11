dae7267167ecd92d964ab820f98201e58f46ce02
vivitruong <vivitruong1506@gmail.com>
2023-09-10 19:27:20 -0500

    fixing merge again

--- react-app/src/components/StockPurchaseForm/index.js
+++ react-app/src/components/StockPurchaseForm/index.js
@@ -126,7 +126,6 @@ function PurchaseStockForm({ average, isLoaded, change }) {
   return (
     isLoaded && (
       <>
-<<<<<<< HEAD
       <div className="form-con">
         <form
           id={
@@ -191,17 +190,12 @@ function PurchaseStockForm({ average, isLoaded, change }) {
           </label>
           <div className="AddtoPortoliobtn"
             onClick={handleSubmit}
-=======
-        <div className="form-con">
-          <form
->>>>>>> e353f373f339ba0f2d7ec2d8afff11b1b7d3b2c2
             id={
               Object.values(errors).length > 0
                 ? "purchase-form"
                 : "purchase-form"
             }
           >
-<<<<<<< HEAD
             <span>Add to portfolio</span>
           </div>
           <p id="form-buying-power-available">
@@ -230,110 +224,6 @@ function PurchaseStockForm({ average, isLoaded, change }) {
 
 
           {/* <button
-=======
-            <h5 id="form-title">Buy {uppercaseTicker}</h5>
-            {
-              <p id="errors-errors" className='purchase-form-item'>
-                {Object.values(errors).map((error) => (
-                  <li id='error-item'>{error}</li>
-                ))}
-              </p>
-            }
-            <div className="purchase-form-item">
-              <p id="form-field1" className="stock-purchase-item">
-                Order Type
-              </p>{" "}
-              <p className="stock-purchase-item" id="form-field2">
-                Buy Order
-              </p>
-            </div>
-            <div className="purchase-form-item">
-              <label id="form-ticker-label" className="stock-purchase-item">
-                Stock Ticker
-              </label>
-              <input
-                className="stock-purchase-item"
-                id="form-ticker-input"
-                name="symbol"
-                placeholder="Ticker Symbol"
-                value={tickerSymbol}
-                onChange={(e) => setTickerSymbol(e.target.value)}
-                required
-              ></input>
-            </div>
-            <div className="purchase-form-item">
-              <label className="stock-purchase-item" id="form-quantity-label">
-                Quantity
-              </label>
-              <input
-                id="form-quantity-input"
-                name="quantity"
-                placeholder="Quantity"
-                value={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-                required
-              ></input>
-            </div>
-            <div className="purchase-form-item">
-              <label id="form-price-label">Average Price</label>
-              <input
-                id="form-price-input"
-                name="Average price"
-                placeholder={average}
-                value={avgPrice}
-                onChange={(e) => setAvgPrice(e.target.value)}
-                required
-              ></input>
-            </div>
-            <div className="purchase-form-item">
-              <label id="form-purchaseIn-label">Purchase In</label>
-              <select name="purchaseIn" id="form-purchaseIn-input">
-                <option value="shares">Shares</option>
-                <option value="shares">Dollars</option>
-                <option value="shares">Cryptos</option>
-              </select>
-            </div>
-            <div
-              ref={submitRef}
-              onClick={handleSubmit}
-              id={
-                change === "+"
-                  ? "form-submit-button"
-                  : "form-submit-button-minus"
-              } className='purchase-form-item'
-            >
-              <span>Add to portfolio</span>
-            </div>{" "}
-            <div className="buying-purchase-form-item">
-              <label id="form-estimated-price">
-                {" "}
-                {estimate > 0
-                  ? `Estimated Price is $${estimate.toLocaleString("en-US")}`
-                  : ""}
-              </label>
-
-              <p id="form-buying-power-available">
-                Buying Power Available $
-                {currentUser?.buyingPower ? currentUser?.buyingPower : 0}
-              </p>
-            </div>
-            <div id="modal-form">
-              <div>{checkModal()}</div>
-            </div>
-            <button onClick={() => setShow(true)} className="addTolist">
-              <span>Add to Watchlist</span>
-            </button>
-            <Modal
-              title={`Add ${ticker} to a Watchlist ?`}
-              show={show}
-              onClose={() => setShow(false)}
-            >
-              <>
-                <StockList assetID={stockId} assetSymbol={ticker} />
-              </>
-            </Modal>
-            {/* <button
->>>>>>> e353f373f339ba0f2d7ec2d8afff11b1b7d3b2c2
             className={
               change === "+"
                 ? "form-add-to-watchlist"
