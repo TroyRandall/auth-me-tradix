16039d2e2245dfd8db45a42a3272d770974fa862
TroyRandall <troyrandall18@gmail.com>
2023-10-21 13:18:44 -0400

    updated formatting function for portfolio value, added stockInfo to useEffect

--- react-app/src/components/DeletePortfolio/index.js
+++ react-app/src/components/DeletePortfolio/index.js
@@ -15,64 +15,69 @@ function DeletePortfolioForm({ price, reset, setStocksIsLoaded }) {
   const currentUser = useSelector((state) => state.session.user);
   const portfolios = useSelector((state) => state.portfolios[userId]);
   const stockInfo = useSelector((state) => state.stocks);
-  const [value, setValue] = useState(0)
+  const [value, setValue] = useState(0);
   const [toggle, setToggle] = useState(false);
   const [errors, setErrors] = useState(false);
 
   useEffect(() => {
-    let formatted = []
-    Object.values(portfolios).forEach((portfolio) => {
-      if(!portfolio?.sold_at){
-        formatted.push({
-          'name': portfolio?.name,
-          'quantity': portfolio?.quantity
-        })
-      }
-    })
-    let count = 0;
-    formatted.forEach((ticker) => {
-      if(stockInfo?.[ticker?.name]){
-           count = count + (Object.values(stockInfo[ticker?.name]['Time Series (Daily)']).reverse()[0]['4. close'] * ticker?.quantity)
-      }
-
-    })
-    setValue(count);
-  }, [stockInfo])
+    if (Object.values(stockInfo).length > 1) {
+      let formatted = [];
+      Object.values(portfolios).forEach((portfolio) => {
+        if (!portfolio?.sold_at) {
+          formatted.push({
+            name: portfolio?.name,
+            quantity: portfolio?.quantity,
+          });
+        }
+      });
+      let count = 0;
+      formatted.forEach((ticker) => {
+        count =
+          count +
+          Object.values(
+            stockInfo[ticker?.name]["Time Series (Daily)"]
+          ).reverse()[0]["4. close"] *
+            ticker?.quantity;
+      });
+      setValue(count);
+    }
+  }, [stockInfo]);
 
   if (!currentUser) history.push("/login");
 
-
-  const cancelModal =  (e) => {
+  const cancelModal = (e) => {
     const overlay = document.getElementById("overlay");
     const yesButton = document.getElementById("confirm-portfolio-reset");
     const noButton = document.getElementById("deny-portfolio-reset");
-    if (e.target === overlay || e.target === noButton){
-        setToggle(false)
-        setErrors(false)
+    if (e.target === overlay || e.target === noButton) {
+      setToggle(false);
+      setErrors(false);
     }
-
   };
 
-  const handleDelete = async(e) => {
-    e.preventDefault()
-      if(!portfolios?.length){
-        setErrors({...errors, 'size': 'You Have No Stocks To Sell, Your Portfolio Is Brand New'})
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    if (!portfolios?.length) {
+      setErrors({
+        ...errors,
+        size: "You Have No Stocks To Sell, Your Portfolio Is Brand New",
+      });
+      return null;
+    }
+    const response = dispatch(
+      portfolioActions.deletePortfolioItem(userId, value)
+    ).then(async (res) => {
+      const data = res;
+
+      if (data && data.errors) {
+        setErrors(data.errors);
         return null;
       }
-      const response =  dispatch(
-        portfolioActions.deletePortfolioItem(userId, value)
-      ).then(async (res) => {
-        const data = res;
-
-        if (data && data.errors) {
-          setErrors(data.errors);
-          return null;
-        }
-      });
-      dispatch(authenticate());
-      setToggle(false);
-      return response;
-   }
+    });
+    dispatch(authenticate());
+    setToggle(false);
+    return response;
+  };
 
   const changeToggle = (e) => {
     e.preventDefault();
@@ -88,9 +93,14 @@ function DeletePortfolioForm({ price, reset, setStocksIsLoaded }) {
           <div id="delete-portfolio">
             <div>
               {Object.values(errors).map((error) => {
-                return <li className="delete-portfolio-items" id="errors-errors-delete">
-                  {error}
-                </li>;
+                return (
+                  <li
+                    className="delete-portfolio-items"
+                    id="errors-errors-delete"
+                  >
+                    {error}
+                  </li>
+                );
               })}
             </div>
             <h3 className="delete-portfolio-items" id="delete-form-title">
